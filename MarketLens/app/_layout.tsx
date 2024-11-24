6f849902e2687d9c78526c0ca34aaaf5a4a958a1
Datakokk <fiodorelzarster@gmx.com>
2024-11-24 07:10:22 +0100

    Se ha instalado y configurado NativeWind v4

---
+++ MarketLens/app/_layout.tsx
@@ -0,0 +1,8 @@
+// Import your global CSS file
+import "./global.css";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return <Slot />;
+};
+export default RootLayout;
---
+++ MarketLens/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-3xl font-bold text-blue-950">Hi Verden</Text>
+    </View>
+  );
+};
+
+export default App;
